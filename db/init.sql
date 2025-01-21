156b517f1dab61b79129ead97b7efe6e9cd88fb0
Dessapt-Corentin <dessaptcorentin66@gmail.com>
2025-01-21 16:46:00 +0100

    sauvegarde de bdd

--- db/init.sql
+++ db/init.sql
@@ -1,9 +1,9 @@
-/*!999999\- enable the sandbox mode */ 
--- MariaDB dump 10.19-11.4.2-MariaDB, for debian-linux-gnu (x86_64)
+/*M!999999\- enable the sandbox mode */ 
+-- MariaDB dump 10.19-11.6.2-MariaDB, for debian-linux-gnu (x86_64)
 --
 -- Host: localhost    Database: symfony
 -- ------------------------------------------------------
--- Server version	11.4.2-MariaDB-ubu2404
+-- Server version	11.6.2-MariaDB-ubu2404
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -15,6 +15,430 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*M!100616 SET @OLD_NOTE_VERBOSITY=@@NOTE_VERBOSITY, NOTE_VERBOSITY=0 */;
+
+--
+-- Table structure for table `age`
+--
+
+DROP TABLE IF EXISTS `age`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `age` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `label` varchar(50) NOT NULL,
+  `image_path` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `age`
+--
+
+LOCK TABLES `age` WRITE;
+/*!40000 ALTER TABLE `age` DISABLE KEYS */;
+INSERT INTO `age` VALUES
+(1,'7','pegi7.png'),
+(2,'12','pegi12.png'),
+(3,'16','pegi16.png'),
+(4,'18','pegi18.png');
+/*!40000 ALTER TABLE `age` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `doctrine_migration_versions`
+--
+
+DROP TABLE IF EXISTS `doctrine_migration_versions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `doctrine_migration_versions` (
+  `version` varchar(191) NOT NULL,
+  `executed_at` datetime DEFAULT NULL,
+  `execution_time` int(11) DEFAULT NULL,
+  PRIMARY KEY (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `doctrine_migration_versions`
+--
+
+LOCK TABLES `doctrine_migration_versions` WRITE;
+/*!40000 ALTER TABLE `doctrine_migration_versions` DISABLE KEYS */;
+INSERT INTO `doctrine_migration_versions` VALUES
+('DoctrineMigrations\\Version20250121143431','2025-01-21 14:34:38',215);
+/*!40000 ALTER TABLE `doctrine_migration_versions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `film`
+--
+
+DROP TABLE IF EXISTS `film`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `film` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `age_id` int(11) DEFAULT NULL,
+  `note_id` int(11) DEFAULT NULL,
+  `titre` varchar(50) NOT NULL,
+  `synopsis` longtext NOT NULL,
+  `date_sortie` date NOT NULL,
+  `image_path` varchar(255) NOT NULL,
+  `time` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_8244BE2226ED0855` (`note_id`),
+  KEY `IDX_8244BE22CC80CD12` (`age_id`),
+  CONSTRAINT `FK_8244BE2226ED0855` FOREIGN KEY (`note_id`) REFERENCES `note` (`id`),
+  CONSTRAINT `FK_8244BE22CC80CD12` FOREIGN KEY (`age_id`) REFERENCES `age` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `film`
+--
+
+LOCK TABLES `film` WRITE;
+/*!40000 ALTER TABLE `film` DISABLE KEYS */;
+INSERT INTO `film` VALUES
+(1,3,1,'Les Affranchis','Le film retrace la vie de Henry Hill, un petit voyou de Brooklyn, depuis son adolescence jusqu’à son arrestation pour trafic de drogue en passant par son ascension dans la mafia.','1990-09-19','affranchis.jpg','145'),
+(2,4,2,'Le Parrain','Le film raconte l’histoire de la famille Corleone, une famille de la mafia sicilienne installée à New York.','1972-09-14','parrain.jpg','175'),
+(3,3,3,'Taxi Driver','Le film raconte l’histoire de Travis Bickle, un vétéran de la guerre du Vietnam devenu chauffeur de taxi à New York.','1976-02-09','taxi_driver.jpg','115'),
+(4,4,4,'Pulp Fiction','Le film est une suite de plusieurs histoires qui se croisent et s’entrecroisent.','1994-10-14','pulp_fiction.jpg','149');
+/*!40000 ALTER TABLE `film` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `film_genre`
+--
+
+DROP TABLE IF EXISTS `film_genre`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `film_genre` (
+  `film_id` int(11) NOT NULL,
+  `genre_id` int(11) NOT NULL,
+  PRIMARY KEY (`film_id`,`genre_id`),
+  KEY `IDX_1A3CCDA8567F5183` (`film_id`),
+  KEY `IDX_1A3CCDA84296D31F` (`genre_id`),
+  CONSTRAINT `FK_1A3CCDA84296D31F` FOREIGN KEY (`genre_id`) REFERENCES `genre` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `FK_1A3CCDA8567F5183` FOREIGN KEY (`film_id`) REFERENCES `film` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `film_genre`
+--
+
+LOCK TABLES `film_genre` WRITE;
+/*!40000 ALTER TABLE `film_genre` DISABLE KEYS */;
+INSERT INTO `film_genre` VALUES
+(1,1),
+(2,1),
+(3,1),
+(4,1);
+/*!40000 ALTER TABLE `film_genre` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `film_personne`
+--
+
+DROP TABLE IF EXISTS `film_personne`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `film_personne` (
+  `film_id` int(11) NOT NULL,
+  `personne_id` int(11) NOT NULL,
+  PRIMARY KEY (`film_id`,`personne_id`),
+  KEY `IDX_AD490686567F5183` (`film_id`),
+  KEY `IDX_AD490686A21BD112` (`personne_id`),
+  CONSTRAINT `FK_AD490686567F5183` FOREIGN KEY (`film_id`) REFERENCES `film` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `FK_AD490686A21BD112` FOREIGN KEY (`personne_id`) REFERENCES `personne` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `film_personne`
+--
+
+LOCK TABLES `film_personne` WRITE;
+/*!40000 ALTER TABLE `film_personne` DISABLE KEYS */;
+INSERT INTO `film_personne` VALUES
+(1,3),
+(2,3),
+(3,3),
+(4,4);
+/*!40000 ALTER TABLE `film_personne` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `genre`
+--
+
+DROP TABLE IF EXISTS `genre`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `genre` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `label` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `genre`
+--
+
+LOCK TABLES `genre` WRITE;
+/*!40000 ALTER TABLE `genre` DISABLE KEYS */;
+INSERT INTO `genre` VALUES
+(1,'Action'),
+(2,'Horreur'),
+(3,'Comique'),
+(4,'Aventure'),
+(5,'Science-fiction'),
+(6,'Fantastique'),
+(7,'Thriller'),
+(8,'Drame'),
+(9,'Policier'),
+(10,'Historique'),
+(11,'Animation'),
+(12,'Documentaire'),
+(13,'Biopic'),
+(14,'Musical'),
+(15,'Guerre'),
+(16,'Western'),
+(17,'Romantique'),
+(18,'Comédie musicale'),
+(19,'Sport'),
+(20,'Jeunesse'),
+(21,'Famille'),
+(22,'Educatif'),
+(23,'Autre');
+/*!40000 ALTER TABLE `genre` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `messenger_messages`
+--
+
+DROP TABLE IF EXISTS `messenger_messages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `messenger_messages` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `body` longtext NOT NULL,
+  `headers` longtext NOT NULL,
+  `queue_name` varchar(190) NOT NULL,
+  `created_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
+  `available_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
+  `delivered_at` datetime DEFAULT NULL COMMENT '(DC2Type:datetime_immutable)',
+  PRIMARY KEY (`id`),
+  KEY `IDX_75EA56E0FB7336F0` (`queue_name`),
+  KEY `IDX_75EA56E0E3BD61CE` (`available_at`),
+  KEY `IDX_75EA56E016BA31DB` (`delivered_at`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `messenger_messages`
+--
+
+LOCK TABLES `messenger_messages` WRITE;
+/*!40000 ALTER TABLE `messenger_messages` DISABLE KEYS */;
+/*!40000 ALTER TABLE `messenger_messages` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `note`
+--
+
+DROP TABLE IF EXISTS `note`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `note` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `media_note` int(11) NOT NULL,
+  `viewer_note` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `note`
+--
+
+LOCK TABLES `note` WRITE;
+/*!40000 ALTER TABLE `note` DISABLE KEYS */;
+INSERT INTO `note` VALUES
+(1,14,13),
+(2,13,13),
+(3,10,11),
+(4,14,11),
+(5,12,13),
+(6,17,17),
+(7,15,13),
+(8,11,13),
+(9,17,17),
+(10,13,16),
+(11,12,17),
+(12,17,20),
+(13,16,16),
+(14,14,14);
+/*!40000 ALTER TABLE `note` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `personne`
+--
+
+DROP TABLE IF EXISTS `personne`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `personne` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(50) NOT NULL,
+  `prenom` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `personne`
+--
+
+LOCK TABLES `personne` WRITE;
+/*!40000 ALTER TABLE `personne` DISABLE KEYS */;
+INSERT INTO `personne` VALUES
+(1,'De Niro','Robert'),
+(2,'Pacino','Al'),
+(3,'Scorsese','Martin'),
+(4,'Tarantino','Quentin'),
+(5,'Eastwood','Clint'),
+(6,'Coppola','Francis Ford'),
+(7,'Ford','Harrison'),
+(8,'Hitchcock','Alfred'),
+(9,'Spielberg','Steven'),
+(10,'Kubrick','Stanley'),
+(11,'Lynch','David');
+/*!40000 ALTER TABLE `personne` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `personne_profession`
+--
+
+DROP TABLE IF EXISTS `personne_profession`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `personne_profession` (
+  `personne_id` int(11) NOT NULL,
+  `profession_id` int(11) NOT NULL,
+  PRIMARY KEY (`personne_id`,`profession_id`),
+  KEY `IDX_409A456DA21BD112` (`personne_id`),
+  KEY `IDX_409A456DFDEF8996` (`profession_id`),
+  CONSTRAINT `FK_409A456DA21BD112` FOREIGN KEY (`personne_id`) REFERENCES `personne` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `FK_409A456DFDEF8996` FOREIGN KEY (`profession_id`) REFERENCES `profession` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `personne_profession`
+--
+
+LOCK TABLES `personne_profession` WRITE;
+/*!40000 ALTER TABLE `personne_profession` DISABLE KEYS */;
+INSERT INTO `personne_profession` VALUES
+(1,1),
+(1,2),
+(2,1),
+(2,3),
+(3,2),
+(3,4),
+(4,2),
+(4,3),
+(5,1),
+(5,2),
+(5,4),
+(6,2),
+(6,4),
+(7,1),
+(8,2),
+(9,2),
+(9,4),
+(10,2),
+(10,3),
+(11,2),
+(11,3);
+/*!40000 ALTER TABLE `personne_profession` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `profession`
+--
+
+DROP TABLE IF EXISTS `profession`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `profession` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `label` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `profession`
+--
+
+LOCK TABLES `profession` WRITE;
+/*!40000 ALTER TABLE `profession` DISABLE KEYS */;
+INSERT INTO `profession` VALUES
+(1,'Acteur'),
+(2,'Réalisateur'),
+(3,'Scénariste'),
+(4,'Producteur'),
+(5,'Compositeur'),
+(6,'Monteur'),
+(7,'Directeur de la photographie'),
+(8,'Ingénieur du son'),
+(9,'Costumier'),
+(10,'Maquilleur'),
+(11,'Cascadeur'),
+(12,'Autre');
+/*!40000 ALTER TABLE `profession` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(180) NOT NULL,
+  `roles` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '(DC2Type:json)' CHECK (json_valid(`roles`)),
+  `password` varchar(255) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_IDENTIFIER_EMAIL` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES
+(1,'admin@admin.com','[\"ROLE_ADMIN\"]','$2y$13$xspZj9fZtbAMPy7d96wIL..oKkP6mcVOBjyJUZucFAuLixXZFc.de','admin');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -25,4 +449,4 @@
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*M!100616 SET NOTE_VERBOSITY=@OLD_NOTE_VERBOSITY */;
 
--- Dump completed on 2024-07-05  8:47:15
+-- Dump completed on 2025-01-21 15:45:23
